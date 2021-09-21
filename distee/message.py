871f35306ad346e91e84ed12ed3dd8e02293e69c
Teekeks <info@teawork.de>
2021-09-21 09:48:54 +0200

    added embed data to message

--- distee/message.py
+++ distee/message.py
@@ -1,7 +1,7 @@
 from .utils import Snowflake
 from .channel import TextChannel
 from .guild import Guild, Member
-from typing import Optional, Union
+from typing import Optional, Union, List
 from .user import User
 
 
@@ -23,6 +23,8 @@ class Message(Snowflake):
             if self.guild is not None else None
         if self.author is None and self._client is not None:
             self.author = self._client.get_user(self.author_id)
+        self.embeds: Optional[List] = args.get('embeds')
+        # FIXME implement all of the message object https://discord.com/developers/docs/resources/channel#message-object
 
     @property
     def jump_url(self):
