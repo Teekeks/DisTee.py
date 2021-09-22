38ed978f73a2bd63523dea60277a79e2ed288dea
Teekeks <info@teawork.de>
2021-09-23 01:31:12 +0200

    added request error handling, retries and rate limit handling

--- distee/errors.py
+++ distee/errors.py
@@ -1,5 +1,54 @@
 
+def flatten_error_dict(d, key=''):
+    items = []
+    for k, v in d.items():
+        new_key = key + '.' + k if key else k
+
+        if isinstance(v, dict):
+            try:
+                _errors = v['_errors']
+            except KeyError:
+                items.extend(flatten_error_dict(v, new_key).items())
+            else:
+                items.append((new_key, ' '.join(x.get('message', '') for x in _errors)))
+        else:
+            items.append((new_key, v))
+
+    return dict(items)
+
+
 class HTTPException(Exception):
+
+    def __init__(self, response, message):
+        self.response = response
+        self.status = response.status
+        if isinstance(message, dict):
+            self.code = message.get('code', 0)
+            base = message.get('message', '')
+            errors = message.get('errors')
+            if errors:
+                errors = flatten_error_dict(errors)
+                helpful = '\n'.join('In %s: %s' % t for t in errors.items())
+                self.text = f'{base}\n{helpful}'
+            else:
+                self.text = base
+        else:
+            self.text = message
+            self.code = 0
+
+        super().__init__(f'{self.status} {self.response.reason} (error code: {self.code}) {self.text}')
+    pass
+
+
+class Forbidden(HTTPException):
+    pass
+
+
+class NotFound(HTTPException):
+    pass
+
+
+class DiscordServerError(HTTPException):
     pass
 
 
--- distee/http.py
+++ distee/http.py
@@ -5,7 +5,7 @@ from typing import Optional, Iterable, Dict, Any
 import aiohttp
 from aiohttp import ClientSession, ClientResponse
 from . import utils
-from .errors import HTTPException, GatewayNotFound
+from .errors import HTTPException, GatewayNotFound, Forbidden, NotFound, DiscordServerError
 import logging
 from pprint import pprint
 
@@ -26,14 +26,41 @@ class Route:
         self.method = method
         self.url = self.BASE_URL + self.path
 
+        self.channel_id = parameters.get('channel_id')
+        self.guild_id = parameters.get('guild_id')
+
+    @property
+    def bucket(self):
+        return f'{self.channel_id}:{self.guild_id}:{self.path}'
+
+
+class MaybeUnlock:
+
+    def __init__(self, lock):
+        self.lock = lock
+        self._unlock = True
+
+    def __enter__(self):
+        return self
+
+    def defer(self):
+        self._unlock = False
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._unlock:
+            self.lock.release()
+
 
 class HTTPClient:
 
     def __init__(self, loop=None):
-        self.loop: asyncio.get_event_loop() if loop is None else loop
+        self.loop = asyncio.get_event_loop() if loop is None else loop
         self.user_agent = 'DisTee v{version}'.format(version=utils.VERSION)
         self.__session: Optional[ClientSession] = None
         self.token: Optional[str] = None
+        self._locks = {}
+        self._global_lock_over = asyncio.Event()
+        self._global_lock_over.set()
 
     async def close(self):
         if self.__session:
@@ -68,7 +95,14 @@ class HTTPClient:
                       form: Optional[Iterable[Dict[str, Any]]] = None,
                       **kwargs):
         method = route.method
+        bucket = route.bucket
         url = route.url
+
+        lock = self._locks.get(bucket)
+        if lock is None:
+            lock = asyncio.Lock()
+            self._locks[bucket] = lock
+
         headers = {
             'User-Agent': self.user_agent,
             'X-RateLimit-Precision': 'millisecond',
@@ -87,15 +121,68 @@ class HTTPClient:
                 form_data.add_field(**p)
             kwargs['data'] = form_data
 
-        try:
-            async with self.__session.request(method=method, url=url, **kwargs) as r:
-                logging.debug(f'{method} {url} with {str(kwargs.get("data"))} has returned {r.status}')
-                data = await get_json_or_str(r)
-                if 300 > r.status >= 200:
-                    return data
-                logging.error('request failed')
-                pprint(data)
-        except Exception:
-            logging.exception('Request failed with Exception')
-        # FIXME propper error handling & retry
-        pass
+        # wait for a global api lock to be over :(
+        if not self._global_lock_over.is_set():
+            await self._global_lock_over.wait()
+
+        await lock.acquire()
+        with MaybeUnlock(lock) as maybe_unlock:
+            for tries in range(5):
+                try:
+                    async with self.__session.request(method=method, url=url, **kwargs) as r:
+                        logging.debug(f'{method} {url} with {str(kwargs.get("data"))} has returned {r.status}')
+                        data = await get_json_or_str(r)
+
+                        remaining = r.headers.get('X-Ratelimit-Remaining')
+
+                        if remaining == '0' and r.status != 429:
+                            # bucket depleted :(
+                            delta = float(r.headers.get('X-Ratelimit-Reset-After'))
+                            logging.debug(f'A rate limit bucket has been exhausted (bucket: {bucket}, retry: {delta})')
+                            maybe_unlock.defer()
+                            self.loop.call_later(delta, lock.release)
+
+                        if 300 > r.status >= 200:
+                            return data
+
+                        if r.status == 429:
+                            if not r.headers.get('Via'):
+                                # this is cloudflare :(
+                                raise HTTPException(r, data)
+
+                            retry_after = float(data['retry_after']) / 1000.0
+                            logging.warning(f'We are being rate limited. Retrying in {retry_after,.2} seconds. Bucket: {bucket}')
+                            is_global = data.get('global', False)
+                            if is_global:
+                                logging.warning(f'Global rate limit has been hit. Retrying in {retry_after,.2} seconds.')
+                                self._global_lock_over.clear()
+                            await asyncio.sleep(retry_after)
+                            logging.debug(f'Done waiting for rate limit, retrying now...')
+                            if is_global:
+                                self._global_lock_over.set()
+                                logging.debug('Global rate limit is over!')
+                            continue
+
+                        # server error -> retry
+                        if r.status in (500, 502):
+                            await asyncio.sleep(1 + tries * 2)
+                            continue
+
+                        if r.status == 403:
+                            raise Forbidden(r, data)
+
+                        if r.status == 404:
+                            raise NotFound(r, data)
+
+                        if r.status == 503:
+                            raise DiscordServerError(r, data)
+                        else:
+                            raise HTTPException(r, data)
+                except OSError as e:
+                    if tries < 4 and e.errno in (54, 10054):
+                        continue
+                    raise
+
+            if r.status >= 500:
+                raise DiscordServerError(r, data)
+            raise HTTPException(r, data)
