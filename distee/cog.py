64a744ea2cd60dd9d71632d4f392bc03e410e470
Teekeks <info@teawork.de>
2021-10-27 22:27:25 +0200

    maybe like this

--- distee/cog.py
+++ distee/cog.py
@@ -3,37 +3,40 @@ from typing import Union, Optional
 from .enums import Event
 
 
-def event(name: Union[str, Event]):
-    def decorator(self, func):
-        n = name.value if isinstance(name, Event) else name
-        if self.client._event_listener.get(n) is None:
-            self.client._event_listener[n] = []
-        self.client._event_listener[n].append(func)
-        return func
 
-    return decorator
-
-
-def raw_event(self, name: str):
-    def decorator(func):
-        self.client.register_raw_gateway_event_listener(name, func)
-        return func
-
-    return decorator
-
-
-def interaction_handler(self,
-                        custom_id: Optional[str] = None):
-    def decorator(func):
-        self.client._interaction_handler[custom_id] = func
-        return func
-
-    return decorator
 
 
 class Cog:
 
     def __init__(self, client: Client):
         self.client: Client = client
+
+
+    @classmethod
+    def event(cls, name: Union[str, Event]):
+        def decorator(func):
+            self = func.__self__
+            n = name.value if isinstance(name, Event) else name
+            if self.client._event_listener.get(n) is None:
+                self.client._event_listener[n] = []
+            self.client._event_listener[n].append(func)
+            return func
+
+        return decorator
+
+    def raw_event(self, name: str):
+        def decorator(func):
+            self.client.register_raw_gateway_event_listener(name, func)
+            return func
+
+        return decorator
+
+    def interaction_handler(self,
+                            custom_id: Optional[str] = None):
+        def decorator(func):
+            self.client._interaction_handler[custom_id] = func
+            return func
+
+        return decorator
 
 
