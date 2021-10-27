e297bf4e61971e5a34eedaf86a14e4552f3f127b
Teekeks <info@teawork.de>
2021-10-27 22:09:07 +0200

    added cog

---
+++ distee/cog.py
@@ -0,0 +1,32 @@
+from .client import Client
+from typing import Union, Optional
+from .enums import Event
+
+
+class Cog:
+
+    def __init__(self, client: Client):
+        self.client: Client = client
+
+    def event(self, name: Union[str, Event]):
+        def decorator(func):
+            n = name.value if isinstance(name, Event) else name
+            if self.client._event_listener.get(n) is None:
+                self.client._event_listener[n] = []
+            self.client._event_listener[n].append(func)
+            return func
+
+        return decorator
+
+    def raw_event(self, name: str):
+        def decorator(func):
+            self.client.register_raw_gateway_event_listener(name, func)
+            return func
+        return decorator
+
+    def interaction_handler(self,
+                            custom_id: Optional[str] = None):
+        def decorator(func):
+            self.client._interaction_handler[custom_id] = func
+            return func
+        return decorator
