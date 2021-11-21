3b3f4821798642ecb31fc43125dda99171ad9a45
Teekeks <info@teawork.de>
2021-11-21 16:38:06 +0100

    some reorganization

--- distee/abc.py
+++ distee/abc.py
@@ -1,7 +1,8 @@
-from typing import Optional, List, TYPE_CHECKING
+from typing import Optional, List, TYPE_CHECKING, Union
 import json
 from distee.http import Route
 from distee.message import Message
+from distee.utils import Snowflake
 
 if TYPE_CHECKING:
     from distee.channel import MessageableChannel
@@ -41,5 +42,33 @@ class Messageable:
         d = await self._client.http.request(Route('POST',
                                                   '/channels/{channel_id}/messages',
                                                   channel_id=channel.id),
+                                            form=form)
+        return Message(**d, _client=channel._client)
+
+    async def edit_message(self, msg_id: Optional[Union[int, Snowflake]],
+                           content: str = None,
+                           tts: bool = False,
+                           reply_to: 'Message' = None,
+                           embeds: Optional[List[dict]] = None,
+                           components: Optional[List] = None,
+                           allowed_mentions: Optional[dict] = None) -> 'Message':
+        payload = {'tts': tts}
+        channel = await self._get_channel()
+        form = []
+        if content is not None:
+            payload['content'] = content
+        if reply_to is not None:
+            payload['message_reference'] = self._get_reference(reply_to)
+        if embeds is not None:
+            payload['embeds'] = embeds
+        if components is not None:
+            payload['components'] = components
+        if allowed_mentions is not None:
+            payload['allowed_mentions'] = allowed_mentions
+        form.append({'name': 'payload_json', 'value': json.dumps(payload)})
+        d = await self._client.http.request(Route('PATCH',
+                                                  '/channels/{channel_id}/messages{message_id}',
+                                                  channel_id=channel.id,
+                                                  message_id=msg_id),
                                             form=form)
         return Message(**d, _client=channel._client)
