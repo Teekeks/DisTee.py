4616b5947bafbb0811bc277fe114413e3bdafa7d
Teekeks <info@teawork.de>
2022-02-10 14:27:41 +0100

    fixed import

--- distee/components.py
+++ distee/components.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Dict
 
-from enums import ComponentType, ButtonStyle, TextInputType
+from .enums import ComponentType, ButtonStyle, TextInputType
 
 ########################################################################################################################
 # BASE
